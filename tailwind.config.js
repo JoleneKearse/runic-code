30da416f3943f3910781e84a4f3519a40f3eb593
JoleneKearse <pharaohnutz@gmail.com>
2024-11-06 11:57:56 -0500

    chore: assemble neutral color warriors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ export default {
 	content: ["./index.html", "./src/**/*.{js,jsx}"],
 	theme: {
 		extend: {
+			colors: {
+				neutral: {
+					50: "#F6F4F4",
+					100: "#EFECEB",
+					200: "#E6E1DF",
+					300: "#D9D1CE",
+					400: "#CCC1BD",
+					500: "#BEB0AC",
+					600: "#AF9D98",
+					700: "#9B857E",
+					800: "#7B6760",
+					900: "#483C38",
+					950: "#120F0E",
+				},
+			},
 			backgroundImage: {
 				fyord: "url('/background.jpg')",
 			},
