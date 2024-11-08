5a182bd20aafd83a1611eb3a5878821ffa865dee
JoleneKearse <pharaohnutz@gmail.com>
2024-11-08 08:59:52 -0500

    chore: edit copy according to the orders of the Jarl

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
     <article className="text-center">
       <Title text="Runic Code" />
       <p className="max-w-prose py-10 text-lg font-semibold text-balance md:text-2xl lg:text-3xl">Where the art of coding meets the ancient practice of divining mysteries through runes. Uncover hidden logic and predict the outcomes of code like a true Viking seer.</p>
-      <p className="max-w-prose mb-12 py-6 rounded-lg bg-neutral-alpha-900 text-neutral-200 text-balance md:text-2xl lg:text-3xl">Read a code snippet & pick the multiple choice question that best explains what it's doing!</p>
+      <p className="max-w-prose mb-12 py-6 rounded-lg bg-neutral-alpha-900 text-neutral-200 text-balance md:text-2xl lg:text-3xl">Read a code snippet & pick the multiple choice question that best explains what it's purpose.</p>
       <Button text="Commence" onClick={handleClick} />
     </article>
   )
