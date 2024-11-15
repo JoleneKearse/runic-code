ac577837d1006946cfcd5590a6fd50917e4aa0d3
JoleneKearse <pharaohnutz@gmail.com>
2024-11-15 15:06:48 -0500

    chore: redirect shirkers to quiz page

--- client/src/pages/ResultsPage.jsx
+++ client/src/pages/ResultsPage.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import QuestionContainer from '../components/QuestionContainer'
+import QuestionContainer from "../components/QuestionContainer";
 
 const ResultsPage = ({
   correctAnswers,
@@ -11,6 +10,7 @@ const ResultsPage = ({
   shuffledQuestions,
   error,
 }) => {
+
   const congratulationsMessages = [
     "You know way more JavaScript than us Vikings ever dreamed of!",
     "Your skills are as sharp as a Viking’s axe — keep honing them!",
@@ -25,26 +25,37 @@ const ResultsPage = ({
   ];
 
   return (
-    <article className="h-screen min-w-[319px] w-9/12 w-[500px] max-w-[720px] mx-auto px-2">
-      <div className="mt-6 py-2 px-4 bg-accent-pink rounded-lg">
-        <p className="text-neutral-900 text-3xl font-black md:text-4xl lg:text-5xl">
-          You slayed {correctAnswers} answer{correctAnswers !== 1 ? "s" : ""}!
-        </p>
-        <p className="mt-4 text-neutral-100 text-xl font-black text-balance md:text-2xl lg:text-3xl">
-          {congratulationsMessages[correctAnswers]}
-        </p>
-      </div>
-      <QuestionContainer
-        quizOver={quizOver}
-        setQuizOver={setQuizOver}
-        correctAnswers={correctAnswers}
-        setCorrectAnswers={setCorrectAnswers}
-        userChoices={userChoices}
-        setUserChoices={setUserChoices}
-        shuffledQuestions={shuffledQuestions}
-        error={error}
-      />
-    </article>
+    <>
+      {!quizOver ? (
+        <div className="font-black text-xl lg:text-2xl flex flex-col gap-6 text-center items-center">
+          <p>Scoundrel! You shirk at divining the runes?</p>
+          <p>You must complete them before entering Vallhalla!</p>
+          <img src="/viking-ship.png" alt="viking ship" className="w-32" />
+          <a href="/quiz" className="text-accent-red text-4xl">Go to challenge</a>
+        </div>
+      ) : (
+        <article className="h-screen min-w-[319px] w-9/12 w-[500px] max-w-[720px] mx-auto px-2">
+          <div className="mt-6 py-2 px-4 bg-accent-pink rounded-lg">
+            <p className="text-neutral-900 text-3xl font-black md:text-4xl lg:text-5xl">
+              You slayed {correctAnswers} answer{correctAnswers !== 1 ? "s" : ""}!
+            </p>
+            <p className="mt-4 text-neutral-100 text-xl font-black text-balance md:text-2xl lg:text-3xl">
+              {congratulationsMessages[correctAnswers]}
+            </p>
+          </div>
+          <QuestionContainer
+            quizOver={quizOver}
+            setQuizOver={setQuizOver}
+            correctAnswers={correctAnswers}
+            setCorrectAnswers={setCorrectAnswers}
+            userChoices={userChoices}
+            setUserChoices={setUserChoices}
+            shuffledQuestions={shuffledQuestions}
+            error={error}
+          />
+        </article>
+      )}
+    </>
   )
 }
 
