3510dde79f67bade52500b34ee9e0e271b5e0cad
JoleneKearse <pharaohnutz@gmail.com>
2024-12-13 15:19:39 -0500

    chore: increase warrior speed with manual chunking

--- client/src/components/Code.jsx
+++ client/src/components/Code.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import javascript from "react-syntax-highlighter/dist/esm/languages/prism/javascript";
 import { solarizedlight } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+SyntaxHighlighter.registerLanguage("javascript", javascript);
 
 const Code = ({ code }) => {
   const customStyle = {
@@ -8,13 +11,13 @@ const Code = ({ code }) => {
     backgroundColor: "#483C38",
     maxWidth: "98%",
   };
-  
+
   return (
     <SyntaxHighlighter
       language="javascript"
       style={solarizedlight}
       customStyle={customStyle}
-      showLineNumbers 
+      showLineNumbers
       wrapLines
     >
       {code}
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { analyzer } from "vite-bundle-analyzer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		analyzer({
+			analyzerMode: "server",
+			analyzerPort: 8888,
+			openAnalyzer: true,
+		}),
+	],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vendor: [
+						"react",
+						"react-dom",
+						"react-router-dom",
+					],
+				},
+			},
+		},
+	},
+});
